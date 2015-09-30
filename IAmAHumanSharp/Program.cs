253e12ded9f185fa5acc07ccfc714f6e78d318a1
Alex <thinkinginbinary@bk.ru>
2015-10-01 00:48:30 +0300

    human

---
+++ IAmAHumanSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Humanizer
+{
+    public static class Program
+    {
+        public static Menu Config;
+        public static void Main(string[] args)
+        {
+
+            CustomEvents.Game.OnGameLoad += eventArgs =>
+            {
+                Config = new Menu("IAmAHumanSharp", "iamhumansharp", true);
+                Config.AddItem(new MenuItem("delay", "Delay (in milliseconds):").SetValue(new Slider(50, 0, 100)));
+            };
+            Obj_AI_Base.OnIssueOrder += (sender, eventArgs) =>
+            {
+                if (sender.IsMe)
+                {
+                    if (eventArgs.Order == GameObjectOrder.AttackUnit)
+                    {
+                        Utility.DelayAction.Add(Config.Item("delay").GetValue<Slider>().Value,
+                            () => ObjectManager.Player.IssueOrder(eventArgs.Order, eventArgs.Target));
+                    }
+                    if (eventArgs.Order == GameObjectOrder.MoveTo)
+                    {
+                        Utility.DelayAction.Add(Config.Item("delay").GetValue<Slider>().Value,
+                            () => ObjectManager.Player.IssueOrder(eventArgs.Order, eventArgs.TargetPosition));
+                    }
+                }
+            };
+
+        }
+    }
+}
