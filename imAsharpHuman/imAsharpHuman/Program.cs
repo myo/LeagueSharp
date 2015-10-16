c0a4045152c2d11decd2ccc0e15f0a1b96d5bce4
Alex <thinkinginbinary@bk.ru>
2015-10-17 02:04:48 +0300

    my Humanizer

---
+++ imAsharpHuman/imAsharpHuman/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace imAsharpHuman
+{
+    class Program
+    {
+        static Menu _menu;
+        static Random _random;
+        private static int _lastMoveT = 0;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += gameLoadEventArgs =>
+            {
+                _random = new Random(Environment.TickCount - Utils.GameTimeTickCount);
+                _menu = new Menu("imAsharpHuman", "imasharphumanmenu");
+                _menu.AddItem(new MenuItem("MinClicks", "Min clicks per second").SetValue(new Slider(_random.Next(5, 6), 1, 6)));
+                _menu.AddItem(new MenuItem("MaxClicks", "Max clicks per second").SetValue(new Slider(_random.Next(6, 10), 6, 20)));
+                _menu.AddToMainMenu();
+            };
+            Obj_AI_Base.OnIssueOrder += (sender, issueOrderEventArgs) =>
+            {
+                if (sender.IsMe && Utils.GameTimeTickCount - _lastMoveT < _random.Next(1000 / _menu.Item("MinClicks").GetValue<Slider>().Value, 1000 / _menu.Item("MaxClicks").GetValue<Slider>().Value))
+                {
+                    issueOrderEventArgs.Process = false;
+                }
+                _lastMoveT = Utils.GameTimeTickCount;
+            };
+        }
+    }
+}
