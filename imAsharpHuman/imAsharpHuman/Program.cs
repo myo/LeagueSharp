eae6abaa016d897600717854766036179493fe9b
Alex <thinkinginbinary@bk.ru>
2015-10-17 02:51:25 +0300

    x

--- imAsharpHuman/imAsharpHuman/Program.cs
+++ imAsharpHuman/imAsharpHuman/Program.cs
@@ -25,7 +25,7 @@ namespace imAsharpHuman
             };
             Obj_AI_Base.OnIssueOrder += (sender, issueOrderEventArgs) =>
             {
-                if (sender.IsMe)
+                if (sender.IsMe && issueOrderEventArgs.Order != GameObjectOrder.HoldPosition)
                 {
                     if (Utils.GameTimeTickCount - _lastCommandT <
                         _random.Next(1000/_menu.Item("MaxClicks").GetValue<Slider>().Value,
