705ac83fc14604917a43219f185f38293dca833b
Alex <thinkinginbinary@bk.ru>
2015-10-28 18:10:48 +0200

    x

--- PRADA Vayne/MyUtils/MActivator.cs
+++ PRADA Vayne/MyUtils/MActivator.cs
@@ -495,20 +495,6 @@ namespace PRADA_Vayne.MyUtils
         }
     }
 
-    public enum AttackId
-    {
-        Unknown = -1,
-        Q = 0,
-        W = 1,
-        E = 2,
-        R = 3,
-        King = 4,
-        Basic = 5,
-        Ignite = 6,
-        Tower = 7,
-        Spell = 8
-    }
-
     public enum ItemTypeId
     {
         Offensive = 0,
