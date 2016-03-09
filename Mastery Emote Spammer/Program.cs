84ab2acd727e21dac6bad1f547a258f6e0a913d4
Alex <thinkinginbinary@bk.ru>
2016-03-09 17:08:17 +0200

    order

--- Mastery Emote Spammer/Program.cs
+++ Mastery Emote Spammer/Program.cs
@@ -58,10 +58,10 @@ namespace Mastery_Badge_Spammer
             Menu.AddItem(new MenuItem("afterw", "After W").SetValue(false));
             Menu.AddItem(new MenuItem("aftere", "After E").SetValue(false));
             Menu.AddItem(new MenuItem("afterr", "After R").SetValue(false));
-            Menu.AddItem(new MenuItem("guccimode", "Use GUCCI Pack?").SetValue(true));
-            Menu.AddItem(new MenuItem("bonobomode", "Use Icy Pack?").SetValue(true));
-            Menu.AddItem(new MenuItem("myomode", "Use myo Pack?").SetValue(true));
             Menu.AddItem(new MenuItem("gentlemanmode", "Use GENTLEMAN Pack?").SetValue(true));
+            Menu.AddItem(new MenuItem("myomode", "Use myo Pack?").SetValue(true));
+            Menu.AddItem(new MenuItem("bonobomode", "Use Icy Pack?").SetValue(true));
+            Menu.AddItem(new MenuItem("guccimode", "Use GUCCI Pack?").SetValue(true));
             Menu.AddToMainMenu();
             Random = new Random();
             FlashSlot = ObjectManager.Player.GetSpellSlot("SummonerFlash");
