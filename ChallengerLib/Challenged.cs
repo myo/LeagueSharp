f1e39d989d8c87f11ef691d333ffb2936d06e223
Alex <thinkinginbinary@bk.ru>
2016-03-31 15:16:39 +0300

    copyright the name XD

---
+++ ChallengerLib/Challenged.cs
@@ -0,0 +1,19 @@
+﻿//This name is now copyrighted and patented thx.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengerLib
+{
+    public class Challenged
+    {
+        // ReSharper disable once InconsistentNaming
+        public void SayXD()
+        {
+            LeagueSharp.Game.Say("XD");
+        }
+    }
+}
