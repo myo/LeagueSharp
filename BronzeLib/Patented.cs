3ef534b8fa270cc7384854eb52dbeecf24b9f52b
Alex <thinkinginbinary@bk.ru>
2016-03-31 15:20:00 +0300

    lib for sebby

---
+++ BronzeLib/Patented.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BronzeLib
+{
+    public class Patented
+    {
+        public void Logics()
+        {
+            LeagueSharp.Game.Say("/all zapienkaka");
+        }
+    }
+}
